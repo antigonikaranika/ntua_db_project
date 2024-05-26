a4a3918e9b321d2f843bb38edda7970dd1571319
Antigoni <82677514+hjmjko@users.noreply.github.com>
2024-05-26 23:13:21 +0300

    Update query8.sql

--- Scripts/query8.sql
+++ Scripts/query8.sql
@@ -1,4 +1,4 @@
--- DONE!
+-- DONE!  
 WITH RecipesCooked AS (
 	SELECT Uses.Utensil_ID, Rating.Episode_ID
 	FROM Rating
@@ -19,3 +19,27 @@ MaxTimesUtensilUsed AS (
 )
 SELECT UtensilsPerEpisode.Episode_ID, UtensilsPerEpisode.TimesUtensilUsed
 FROM UtensilsPerEpisode JOIN MaxTimesUtensilUsed ON UtensilsPerEpisode.TimesUtensilUsed = MaxTimesUtensilUsed.MaxCounter;
+
+-- Alternative Query Plan
+
+WITH RecipesCooked AS (
+	SELECT Uses.Utensil_ID, Rating.Episode_ID
+	FROM Rating
+	STRAIGHT_JOIN Recipe ON Rating.Recipe_ID = Recipe.Recipe_ID
+	JOIN Uses ON Recipe.Recipe_ID = Uses.Recipe_ID
+	-- JOIN Utensil ON Uses.Utensil_ID = Utensil.Utensil_ID
+	GROUP BY Rating.Recipe_ID, Rating.Cook_ID, Rating.Episode_ID, Uses.Utensil_ID
+),
+UtensilsPerEpisode AS (
+	SELECT RecipesCooked.Episode_ID, COUNT(*) AS TimesUtensilUsed
+	FROM RecipesCooked
+	GROUP BY RecipesCooked.Episode_ID
+	ORDER BY RecipesCooked.Episode_ID
+),
+MaxTimesUtensilUsed AS (
+	SELECT MAX(TimesUtensilUsed) AS MaxCounter
+	FROM UtensilsPerEpisode
+)
+SELECT UtensilsPerEpisode.Episode_ID, UtensilsPerEpisode.TimesUtensilUsed
+FROM UtensilsPerEpisode JOIN MaxTimesUtensilUsed ON UtensilsPerEpisode.TimesUtensilUsed = MaxTimesUtensilUsed.MaxCounter;
+
