6db5285f156dd3a38e2d5d847bfa9283d5e8f123
Antigoni <82677514+hjmjko@users.noreply.github.com>
2024-05-26 22:43:58 +0300

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,12 @@
 #! /usr/bin/bash
 
+#MariaDB Connector/Python Requirements
 apt -y install libmariadb3 libmariadb-dev
-pip3 install python==3.12.3 numpy networkx mariadb==1.1.10 jupyter
+
+apt -y install mariadb-server
+apt-get -y install mariadb-backup
+mysql_secure_installation
+systemctl start mariadb.service
+
+#If anaconda isn't installed
+#bash ./Anaconda3-2024.02-1-Linux-x86_64.sh
